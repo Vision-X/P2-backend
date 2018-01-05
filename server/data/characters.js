b9bb8f57ddfce7176f0c1328e807c35bc1bc205f
Mike Marlow <michael.s.marlow@gmail.com>
2018-01-04 18:05:30 -0700

    initial backend data object built

---
+++ server/data/characters.js
@@ -0,0 +1,152 @@
+module.exports = {
+  characterSet: [{
+      id: 1,
+      name: 'Doomfist',
+      role: 'Offense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/doomfist/icon-portrait.png'
+    },
+    {
+      id: 2,
+      name: 'Genji',
+      role: 'Offense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/genji/icon-portrait.png'
+    },
+    {
+      id: 3,
+      name: 'Pharah',
+      role: 'Offense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/pharah/icon-portrait.png'
+    },
+    {
+      id: 4,
+      name: 'Reaper',
+      role: 'Offense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/reaper/icon-portrait.png'
+    },
+    {
+      id: 5,
+      name: 'Soldier:76',
+      role: 'Offense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/soldier-76/icon-portrait.png'
+    },
+    {
+      id: 6,
+      name: 'Sombra',
+      role: 'Offense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/sombra/icon-portrait.png'
+    },
+    {
+      id: 7,
+      name: 'Tracer',
+      role: 'Offense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/tracer/icon-portrait.png'
+    },
+    {
+      id: 8,
+      name: 'Bastion',
+      role: 'Defense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/bastion/icon-portrait.png'
+    },
+    {
+      id: 9,
+      name: 'Hanzo',
+      role: 'Defense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/hanzo/icon-portrait.png'
+    },
+    {
+      id: 10,
+      name: 'Junkrat',
+      role: 'Defense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/junkrat/icon-portrait.png'
+    },
+    {
+      id: 11,
+      name: 'Mei',
+      role: 'Defense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/mei/icon-portrait.png'
+    },
+    {
+      id: 12,
+      name: 'Torbjörn',
+      role: 'Defense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/torbjorn/icon-portrait.png'
+    },
+    {
+      id: 13,
+      name: 'Widowmaker',
+      role: 'Defense',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/widowmaker/icon-portrait.png'
+    },
+    {
+      id: 14,
+      name: 'D.Va',
+      role: 'Tank',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/dva/icon-portrait.png'
+    },
+    {
+      id: 15,
+      name: 'Orisa',
+      role: 'Tank',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/orisa/icon-portrait.png'
+    },
+    {
+      id: 16,
+      name: 'Reinhardt',
+      role: 'Tank',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/reinhardt/icon-portrait.png'
+    },
+    {
+      id: 17,
+      name: 'Roadhog',
+      role: 'Tank',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/roadhog/icon-portrait.png'
+    },
+    {
+      id: 18,
+      name: 'Winston',
+      role: 'Tank',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/winston/icon-portrait.png'
+    },
+    {
+      id: 19,
+      name: 'Zarya',
+      role: 'Tank',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/zarya/icon-portrait.png'
+    },
+    {
+      id: 20,
+      name: 'Ana',
+      role: 'Support',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/ana/icon-portrait.png'
+    },
+    {
+      id: 21,
+      name: 'Lucio',
+      role: 'Support',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/lucio/icon-portrait.png'
+    },
+    {
+      id: 22,
+      name: 'Mercy',
+      role: 'Support',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/mercy/icon-portrait.png'
+    },
+    {
+      id: 23,
+      name: 'Moira',
+      role: 'Support',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/moira/icon-portrait.png'
+    },
+    {
+      id: 24,
+      name: 'Symmetra',
+      role: 'Support',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/symmetra/icon-portrait.png'
+    },
+    {
+      id: 25,
+      name: 'Zenyatta',
+      role: 'Support',
+      charImg: 'https://d1u1mce87gyfbn.cloudfront.net/hero/zanyatta/icon-portrait.png'
+    }
+]}
